ad14244f19e39fd55e59a868eaf49799eb4aa5f6
Ujjawal Chatterjee <49064692+yourdevujjawal@users.noreply.github.com>
2020-07-24 18:57:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
-const overviewDescriptionWrapper = document.querySelector(
-  ".overview-description-overview"
-);
+"use strict";
 
-const animatedheightInner = document.querySelector(
-  ".animatedheight-inner-overview"
-);
-
-const readMoreBtn = document.querySelector(
-  ".colapsshoecontentlinkdiv-overview"
-);
-
-const readLessBtn = document.querySelector(
-  ".colapsshoecontentlinkdiv2-overview"
-);
-
-const h2collection = animatedheightInner.querySelectorAll("h2");
-
-let numberOfPara;
+var overviewDescriptionWrapper = document.querySelector(".overview-description-overview");
+var animatedheightInner = document.querySelector(".animatedheight-inner-overview");
+var readMoreBtn = document.querySelector(".colapsshoecontentlinkdiv-overview");
+var readLessBtn = document.querySelector(".colapsshoecontentlinkdiv2-overview");
+var h2collection = animatedheightInner.querySelectorAll("h2");
+var numberOfPara;
 
 function screenWidth() {
   return window.innerWidth;
 }
 
-function handleNumberOfPara(desktopParaCount=2, ipadLandParaCount=desktopParaCount, ipadParaCount=1, mobileParaCount=1) {
-  if(screenWidth() > 1199) {
+function handleNumberOfPara() {
+  var desktopParaCount = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 2;
+  var ipadLandParaCount = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : desktopParaCount;
+  var ipadParaCount = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 1;
+  var mobileParaCount = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 1;
+
+  if (screenWidth() > 1199) {
     numberOfPara = desktopParaCount;
-  }else if (screenWidth() > 1023) {
+  } else if (screenWidth() > 1023) {
     numberOfPara = ipadLandParaCount;
-  }else if (screenWidth() > 767) {
+  } else if (screenWidth() > 767) {
     numberOfPara = ipadParaCount;
   } else {
     numberOfPara = mobileParaCount;
@@ -37,19 +31,16 @@ function handleNumberOfPara(desktopParaCount=2, ipadLandParaCount=desktopParaCou
 window.addEventListener("load", function () {
   handleNumberOfPara(desktopParaCount, ipadLandParaCount, ipadParaCount, mobileParaCount);
   defaultHeight(numberOfPara);
-
   readMoreBtn.addEventListener("click", expandOverviewDescription);
-
   readLessBtn.addEventListener("click", collapseOverviewDescription);
 });
-
 window.addEventListener("resize", function () {
   handleNumberOfPara(desktopParaCount, ipadLandParaCount, ipadParaCount, mobileParaCount);
   defaultHeight(numberOfPara);
 });
 
 function expandOverviewDescription() {
-  overviewDescriptionWrapper.style.height = `${animatedheightInner.offsetHeight}px`;
+  overviewDescriptionWrapper.style.height = "".concat(animatedheightInner.offsetHeight, "px");
   readMoreBtn.style.display = "none";
   readLessBtn.style.display = "block";
 }
@@ -60,18 +51,18 @@ function collapseOverviewDescription() {
   readMoreBtn.style.display = "block";
 }
 
-function defaultHeight(numberOfPara = 2) {
+function defaultHeight() {
+  var numberOfPara = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 2;
   readMoreBtn.style.display = "none";
   readLessBtn.style.display = "none";
+
   if (h2collection.length > 0) {
-    let heightToShow;
+    var heightToShow;
+
     if (numberOfPara < h2collection.length) {
       heightToShow = h2collection[numberOfPara].offsetTop;
       readMoreBtn.style.display = "block";
-    } else if (
-      numberOfPara == h2collection.length ||
-      h2collection.length == 1
-    ) {
+    } else if (numberOfPara == h2collection.length || h2collection.length == 1) {
       heightToShow = animatedheightInner.offsetHeight;
       readMoreBtn.style.display = "none";
       readLessBtn.style.display = "none";
@@ -79,6 +70,7 @@ function defaultHeight(numberOfPara = 2) {
       heightToShow = h2collection[h2collection.length - 1].offsetTop;
       readMoreBtn.style.display = "block";
     }
-    overviewDescriptionWrapper.style.height = `${heightToShow}px`;
+
+    overviewDescriptionWrapper.style.height = "".concat(heightToShow, "px");
   }
 }
